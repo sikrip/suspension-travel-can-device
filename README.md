7adf02080ebcb1f15d82ca7cf9ab21264405c9bf
sikrip@gmail.com <sikrip@gmail.com>
2023-07-06 16:06:50 +0300

    Adapttion from RejsaRubberTrac project in order to use a potentiometer instead of distance sensor. Also incorporates MLK90640.

--- README.md
+++ README.md
@@ -1,11 +1,24 @@
-# Suspension Travel CAN device for RaceChrono
-Using an Adafruit Feather nRF52832 in order to send suspension travel data via
-bluetooth in the RaceChrono app.
+# Suspension Travel & Tyre temp sensor for RaceChrono
+A modified https://github.com/MagnusThome/RejsaRubberTrac that uses a potentiometer instead of a
+distance sensor to measure the suspension travel. It also uses the newer MLX90640 sensor.
 
-Coded based on info found under this repo https://github.com/aollin/racechrono-ble-diy-device
+# Usage
+1. Set the name to match the corner you are going to place each device
+
+   ``#define DEVICE_NAME "RearLeftSensor"``
+2. Place the temp sensor so that the reference pin points to the back of the car
+3. Take note of the potentiometer value at ride height and at full droop. Also measure the travel from
+ride height to droop. Then set the following values accordingly:
+
+    ``#define DROOP_TRAVEL -33``
+
+    ``#define SENSOR_RIDE_HEIGHT 398``
+
+    ``#define SENSOR_DROOP 312``
+4. Added the device as "RejsaRubberTrac" in "other devices" under RaceChrone setup.
 
 ## Safety notice
 This is experimental, use it with caution and at your own risk!
 
 ## License
-This = is [licensed](LICENSE) under the [MIT Licence](http://en.wikipedia.org/wiki/MIT_License).
+This is [licensed](LICENSE) under the [MIT Licence](http://en.wikipedia.org/wiki/MIT_License).
--- main/main.ino
+++ main/main.ino
@@ -1,25 +1,26 @@
-#include <bluefruit.h>
+#include <Arduino.h>
+#include <Wire.h>
+#include "MLX90640_API.h"
+#include "MLX90640_I2C_Driver.h"
+#include "ble_gatt.h"
+#include "adc_vbat.h"
 
-#define DEVICE_NAME "FR_S_11"
-#define DATA_REPORT_INTERVAL_MS 100
-#define SUSPENSION_TRAVEL_PID 1
+#define DEVICE_NAME "RearLeftSensor" // Change as needed
 
-#define SENSOR_RIDE_HEIGHT 345
-#define SENSOR_DROOP 294
-
-// Since we cannot report negative values in BLE, droop is defined as 0.
+// Definitions needed for measuring suspension travel using a potentiometer as input.
+// Ride height is defined as 0 travek. Droop will report negative values, bump positive values.
 // Mapping is done using droop and ride height points since it is easier to get
 // these measurements. All values in mm.
-#define RIDE_HEIGHT_TRAVEL 33
-#define DROOP_TRAVEL 0
-#define MIN_TRAVEL 0
+#define RIDE_HEIGHT_TRAVEL 0
+#define MIN_TRAVEL -100
 #define MAX_TRAVEL 100
+#define DISTANCE_SENSOR_PIN A0`
+#define DROOP_TRAVEL -33
+#define SENSOR_RIDE_HEIGHT 398 // the value of the sensor at ride height
+#define SENSOR_DROOP 312 // the sensor value at full droop
 
-#define CAN_IDX 0
-#define SENSOR_PIN A0
-
+// Used to debug using serial monitor.
 #define HAS_DEBUG
-
 #ifdef HAS_DEBUG
 #define debugln Serial.println
 #define debug Serial.print
@@ -30,129 +31,243 @@ void dummy_debug(...) {
 #define debugln dummy_debug
 #endif
 
-unsigned long time_now = 0;
-uint8_t canData[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-int sensorValue;
-int suspensionTravel;
+// MLX90640
+const byte MLX90640_address = 0x33; //Default 7-bit unshifted address of the MLX90640
 
-// BLEService docs: https://learn.adafruit.com/bluefruit-nrf52-feather-learning-guide/bleservice
-BLEService bleService = BLEService(0x1ff8);
+#define TA_SHIFT 8 //Default shift for MLX90640 in open air
 
-// BLECharacteristic docs: https://learn.adafruit.com/bluefruit-nrf52-feather-learning-guide/blecharacteristic
-// RaceChrono uses two BLE characteristics:
+#define TOTAL_PIXELS 768 // 32x24
+static float mlx90640To[TOTAL_PIXELS];
+paramsMLX90640 mlx90640;
 
-// 0x02 to request which PIDs to send, and how frequently
-BLECharacteristic pidRequestsCharacteristic = BLECharacteristic(0x02);
-// 0x01 to be notified of data received for those PIDs
-BLECharacteristic canBusDataCharacteristic = BLECharacteristic(0x01);
+// Sensor should be placed with the reference tab pointing to the back of the car.
+// We always need to have Temp1 => inner most temp, Temp8 => outer most temp
+// For right hand side tyres uncomment to keep the above consistent for all tyres.                  
+// #define RIGHT_SIDE_TYRE     
+                                              
+#define PROTOCOL 0x02
+#define TEMPSCALING 1.00    // Default = 1.00
+#define TEMPOFFSET 0        // Default = 0      NOTE: in TENTHS of degrees Celsius --> TEMPOFFSET 10 --> 1 degree
 
-void initBLE() {
-  Bluefruit.configPrphBandwidth(BANDWIDTH_MAX);
-  Bluefruit.begin();
-  Bluefruit.setName(DEVICE_NAME);
+typedef struct {
+  uint8_t  protocol;         // version of protocol used
+  uint8_t  unused;
+  int16_t  distance;         // millimeters
+  int16_t  temps[8];         // all even numbered temp spots (degrees Celsius x 10)
+} one_t;
 
-  bleService.begin();
 
-  pidRequestsCharacteristic.setProperties(CHR_PROPS_WRITE);
-  pidRequestsCharacteristic.setPermission(SECMODE_NO_ACCESS, SECMODE_OPEN);
-  // pidRequestsCharacteristic.setWriteCallback(handle_racechrono_filter_request);
-  pidRequestsCharacteristic.begin();
+typedef struct {
+  uint8_t  protocol;         // version of protocol used
+  uint8_t  charge;           // percent: 0-100
+  uint16_t voltage;          // millivolts (normally circa 3500-4200)
+  int16_t  temps[8];         // all uneven numbered temp spots (degrees Celsius x 10)
+} two_t;
 
-  canBusDataCharacteristic.setProperties(CHR_PROPS_NOTIFY | CHR_PROPS_READ);
-  canBusDataCharacteristic.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  canBusDataCharacteristic.begin();
 
-  Bluefruit.setTxPower(+4);
-}
+typedef struct {
+  uint8_t  protocol;         // version of protocol used
+  uint8_t  unused;
+  int16_t distance;          // millimeters
+  int16_t  temps[8];         // All 16 temp spots used as pairs of two each, max from each pair goes into these 8 temp values (degrees Celsius x 10)
+} thr_t;
 
-void startBLEAdvertising() {
-  Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
-  Bluefruit.Advertising.addTxPower();
-  Bluefruit.Advertising.addService(bleService);
-  Bluefruit.Advertising.addName();
-  Bluefruit.Advertising.restartOnDisconnect(true);
 
-  // Fast mode interval: 20 ms, slow mode interval: 152.5 ms.
-  // The numbers specified on the call are multiplied by (0.625 ms) units.
-  Bluefruit.Advertising.setInterval(/* fast= */ 32, /* slow= */ 244);
+one_t datapackOne;
+two_t datapackTwo;
+thr_t datapackThr;
+// The raw value of the potentiometer measuring the ride height
+int potentiometerValue;
 
-  // Timeout for fast mode is 30 seconds.
-  Bluefruit.Advertising.setFastTimeout(30);
-
-  // Start advertising forever.
-  Bluefruit.Advertising.start(/* timeout= */ 0);
-}
-
-void initSensors() {
-  pinMode(SENSOR_PIN, INPUT);
-}
+boolean isTempSensorConnected();
+void tempSensorSetup();
+void measureSuspensionTravel();
+void measureTemps();
+void measureBattery();
+void printStatus(void);
 
 void setup() {
-  uint32_t startTimeMs = millis();
+    uint32_t startTimeMs = millis();
 #ifdef HAS_DEBUG
   Serial.begin(115200);
 #endif
   while (!Serial && millis() - startTimeMs < 1000) {
   }
+  debugln("\nBegin startup");
 
-  debugln("Initializing BLE...");
-  initBLE();
-  startBLEAdvertising();
+  Bluefruit.autoConnLed(false); // DISABLE BLUE BLINK ON CONNECT STATUS
 
-  debugln("BLE initialized, waiting for connection.");
-  waitForConnection();
-}
+  tempSensorSetup();
 
-void waitForConnection() {
-  uint32_t printIteration = 0;
-  while (!Bluefruit.connected()) {
-    if (printIteration < 10) {
-      debug(".");
-      printIteration++;
-    } else {
-      debugln();
-      printIteration = 0;
-    }
-    delay(500);
-  }
-  debugln("Connected.");
-}
+  // SET SOME DEFAULT VALUES IN THE DATA PACKETS
+  datapackOne.distance = 0;
+  datapackOne.protocol = PROTOCOL;
+  datapackTwo.protocol = PROTOCOL;
+  datapackThr.distance = 0;
+  datapackThr.protocol = PROTOCOL;
 
-void sendData(uint32_t pid, const uint8_t *data) {
-  unsigned char buffer[20];
-  buffer[0] = pid & 0xFF;
-  buffer[1] = (pid >> 8) & 0xFF;
-  buffer[2] = (pid >> 16) & 0xFF;
-  buffer[3] = (pid >> 24) & 0xFF;
-  memcpy(buffer + 4, data, 8);
-  // 4 for PID and 8 for payload
-  canBusDataCharacteristic.notify(buffer, 12);
-}
+  // START UP BLUETOOTH
+  debug("Starting bluetooth with MAC address ");
+  Bluefruit.begin();
+  debugln();
+  debug("Device name: ");
+  debugln(DEVICE_NAME);
+  Bluefruit.setName(DEVICE_NAME); 
 
-void reportData() {
- if(millis() >= time_now + DATA_REPORT_INTERVAL_MS){
-     time_now += DATA_REPORT_INTERVAL_MS;
-
-     sensorValue = analogRead(SENSOR_PIN);
-     // map from potentiometer reading to suspension travel
-     suspensionTravel = min(
-       MAX_TRAVEL,
-      max(
-        MIN_TRAVEL,
-        map(sensorValue, SENSOR_RIDE_HEIGHT, SENSOR_DROOP, RIDE_HEIGHT_TRAVEL, DROOP_TRAVEL)
-      )
-     );
-     debugln(String(sensorValue, DEC) + "->" + String(suspensionTravel, DEC));
-     canData[CAN_IDX] = suspensionTravel;
-
-     sendData(SUSPENSION_TRAVEL_PID, canData);
-   }
+  // RUN BLUETOOTH GATT
+  setupMainService();
+  startAdvertising(); 
+  debugln("Running!");
 }
 
 void loop() {
-  if (!Bluefruit.connected()) {
-    debugln("RaceChrono disconnected! Waiting for a new connection.");
-    waitForConnection();
+  
+  measureSuspensionTravel();
+  measureTemps();
+  measureBattery();
+  
+  if (Bluefruit.connected()) {
+    GATTone.notify(&datapackOne, sizeof(datapackOne));
+    GATTtwo.notify(&datapackTwo, sizeof(datapackTwo));
+    GATTthr.notify(&datapackThr, sizeof(datapackThr));
   }
-  reportData();
+
+  printStatus();
+}
+
+//Returns true if the MLX90640 is detected on the I2C bus
+boolean isTempSensorConnected() {
+  Wire.beginTransmission((uint8_t)MLX90640_address);
+  if (Wire.endTransmission() != 0) {
+    return (false); //Sensor did not ACK
+  }
+  return (true);
+}
+
+// Setup MLX90640
+void tempSensorSetup() {
+  debugln("Initializing temp sensor...");
+  Wire.begin();
+  Wire.setClock(400000); //Increase I2C clock speed to 400kHz
+
+  if (!isTempSensorConnected()) {
+    debugln("MLX90640 not detected at default I2C address. Please check wiring. Freezing.");
+    while (1);
+  }
+  debugln("MLX90640 connected!");
+
+  //Get device parameters - We only have to do this once
+  int status;
+  uint16_t eeMLX90640[832];
+  status = MLX90640_DumpEE(MLX90640_address, eeMLX90640);
+  if (status != 0) {
+    debugln("Failed to load system parameters");
+  }
+
+  status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
+  if (status != 0) {
+    debugln("Parameter extraction failed");
+  }
+
+  // Readig as fast as possibe
+  MLX90640_SetRefreshRate(MLX90640_address, 0x04);
+}
+
+void measureTemps() {
+  for (byte x = 0 ; x < 2 ; x++) {//Read both subpages
+    uint16_t mlx90640Frame[834];
+    int status = MLX90640_GetFrameData(MLX90640_address, mlx90640Frame);
+    if (status < 0) {
+      debugln("GetFrame Error: ");
+      debugln(status);
+    }
+
+    float vdd = MLX90640_GetVdd(mlx90640Frame, &mlx90640);
+    float Ta = MLX90640_GetTa(mlx90640Frame, &mlx90640);
+
+    float tr = Ta - TA_SHIFT; //Reflected temperature based on the sensor ambient temperature
+    float emissivity = 0.95;
+
+    MLX90640_CalculateTo(mlx90640Frame, &mlx90640, emissivity, tr, mlx90640To);
+  }
+
+  // Placement of the sensor should be with reference tab of the sensor pointing to the back of the car.
+  // We always make Temp1 the inner most side of the tyre and Temp8 the outer most side
+  // see RIGHT_SIDE_TYRE
+  for (byte tIdx = 0; tIdx < 8; tIdx++) {
+  #ifdef RIGHT_SIDE_TYRE
+    const byte valueIdx = 7 - tIdx;
+  #else
+    const byte valueIdx = tIdx;
+  #endif
+    const byte sensorArrayStartColumn = tIdx * 4;
+    float avgT = 0; // -1000 degress celcius seems ok to start :)
+    for(byte r=0; r<24; r++) {
+        byte pixelIdx = sensorArrayStartColumn + r * 32;
+        avgT += mlx90640To[pixelIdx] + mlx90640To[pixelIdx+1];
+    }
+    datapackOne.temps[valueIdx] = (int16_t) TEMPOFFSET + TEMPSCALING * 10 * (avgT / 48);
+
+    avgT = 0;
+    for(byte r=0; r<24; r++) {
+        byte pixelIdx = sensorArrayStartColumn + 2 + r * 32;
+        avgT += mlx90640To[pixelIdx] + mlx90640To[pixelIdx+1];
+    }
+    datapackTwo.temps[valueIdx] = (int16_t) TEMPOFFSET + TEMPSCALING * 10 * (avgT / 48);
+    datapackThr.temps[valueIdx] = (int16_t) max(datapackOne.temps[valueIdx], datapackTwo.temps[valueIdx]);
+  }
+}
+
+/**
+ * Measures the suspension travel using a potentiometer.
+ */
+void measureSuspensionTravel() {
+  potentiometerValue = analogRead(DISTANCE_SENSOR_PIN);
+  // map from potentiometer reading to suspension travel
+  datapackOne.distance = min(
+    MAX_TRAVEL,
+    max(
+      MIN_TRAVEL,
+      map(potentiometerValue, SENSOR_RIDE_HEIGHT, SENSOR_DROOP, RIDE_HEIGHT_TRAVEL, DROOP_TRAVEL)
+    )
+  );
+}
+
+void measureBattery() {
+  unsigned long now = millis();
+  static unsigned long timer = 60000;  // check every 60 seconds
+  if (now - timer >= 60000) {
+    timer = now;
+    datapackTwo.voltage = getVbat();
+    datapackTwo.charge = lipoPercent(datapackTwo.voltage);
+  }
+}
+
+void printStatus(void) {
+#ifdef HAS_DEBUG
+  static unsigned long then;
+  unsigned long now = millis();
+  debug(1000/(float)(now - then),1); // Print loop speed in Hz
+  debug("Hz\t");
+  then = now;
+
+  debug(datapackTwo.voltage);
+  debug("mV\t");
+  debug(datapackTwo.charge);
+  debug("%\t");
+
+  debug(datapackOne.distance);
+  debug("mm(");
+  debug(potentiometerValue);
+  debug(")\t");
+
+  for (uint8_t i=0; i<8; i++) {
+    debug(datapackOne.temps[i]);
+    debug("\t");
+    debug(datapackTwo.temps[i]);
+    debug("\t");
+  }
+
+  debugln();
+#endif
 }
